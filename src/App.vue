22c4de0a05c20fa4ff23e181884dbb7123fdf655
Matej Valky <enderstorm@outlook.sk>
2017-05-24 23:09:20 +0200

    Bulma + font awesome

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
   margin-top: 60px;
 }
+</style>
+<style lang="scss">
+  // Import bulma.io style
+  @import "~bulma/bulma.sass";
+  // Import site styles
+  @import "./styles/site.scss";
 </style>
